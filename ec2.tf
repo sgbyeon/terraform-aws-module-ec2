7da05b067df267123789e3937912233249cbdb03
sgbyeon <sg.byeon1@gmail.com>
2021-11-30 17:36:30 +0900

    [bsg] updates

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "this" {
   ami = var.ami_id
   instance_type = var.instance_type
+  key_name = var.key_name
 
   network_interface {
     network_interface_id = aws_network_interface.this.id
